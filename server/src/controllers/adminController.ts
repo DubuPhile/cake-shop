6a0ebde1b1de695c2497e15aea6f4e93515231cd
Charles <enagecharles27@gmail.com>
2026-05-07 00:06:32 +0800

    deleteUser

--- server/src/controllers/adminController.ts
+++ server/src/controllers/adminController.ts
@@ -1,20 +1,65 @@
 import { Request, Response } from "express";
 import { prisma } from "../../lib/prisma";
+import { AuthRequest } from "../middleware/verifyJWT";
+
+const safeUserSelect = {
+  userId: true,
+  name: true,
+  email: true,
+  roles: true,
+  isAdmin: true,
+} as const;
 
 export const getUser = async (req: Request, res: Response): Promise<void> => {
   try {
-    const safeUserSelect = {
-      userId: true,
-      name: true,
-      email: true,
-      roles: true,
-      isAdmin: true,
-    } as const;
     const users = await prisma.users.findMany({
       select: safeUserSelect,
     });
     res.json(users);
   } catch (err) {
     res.status(500).json({ message: "Error Fetching Users" });
+  }
+};
+
+export const deleteUser = async (
+  req: AuthRequest,
+  res: Response,
+): Promise<void> => {
+  try {
+    const roles = req.user?.roles;
+    const { id } = req.params;
+
+    if (!id || Array.isArray(id)) {
+      res.status(400).json({ message: "Invalid ID" });
+      return;
+    }
+
+    if (roles !== "ADMIN") {
+      res.status(401).json({ message: "Unauthorized", success: false });
+      return;
+    }
+
+    const foundUser = await prisma.users.findFirst({
+      where: {
+        userId: id,
+      },
+      select: safeUserSelect,
+    });
+    if (!foundUser) {
+      res.status(404).json({ message: "User not Found", success: false });
+      return;
+    }
+    await prisma.users.delete({
+      where: {
+        userId: foundUser.userId,
+      },
+    });
+
+    res.status(200).json({
+      message: `users with an id of ${id} has been deleted`,
+      success: true,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Error Deleting user" });
   }
 };
