ab48c438ff445f55273ed6aad63b8f9ca1fa0261
Charles <enagecharles27@gmail.com>
2026-05-09 00:13:23 +0800

    verifyOTP routes

--- server/src/routes/verifyOTPRoutes.ts
+++ server/src/routes/verifyOTPRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { verifyOTP } from "../controllers/OTPController";
+import { validate } from "../middleware/validate";
+import { verifyOtpSchema } from "../validator/auth.validator";
 
 const router = express.Router();
 
-router.post("/", verifyOTP);
+router.post("/", validate(verifyOtpSchema), verifyOTP);
 
 export default router;
