4231dd806e8f4158f4791d76c47a65229d2257f4
Charles <enagecharles27@gmail.com>
2026-05-09 00:12:50 +0800

    optimized login routes

--- server/src/routes/LoginRoutes.ts
+++ server/src/routes/LoginRoutes.ts
@@ -1,11 +1,10 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/loginController";
+import { loginUser } from "../controllers/loginController";
 import { validate } from "../middleware/validate";
-import { loginSchema, registerSchema } from "../validator/auth.validator";
+import { loginSchema } from "../validator/auth.validator";
 
 const router = express.Router();
 
 router.post("/", validate(loginSchema), loginUser);
-router.post("/register", validate(registerSchema), registerUser);
 
 export default router;
