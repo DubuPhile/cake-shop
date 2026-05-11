8d013584e46f1862ab3b2fc422018c34424efbb6
Charles <enagecharles27@gmail.com>
2026-05-11 18:43:58 +0800

    client first init

---
+++ client/src/app/( dashboard )/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Metrics",
+};
+
+export default function Dashboard() {
+  return <div>Dashboard</div>;
+}
