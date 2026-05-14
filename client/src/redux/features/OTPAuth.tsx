f4aae45a5fea8c5598c0235250be13012ce23aba
Charles <enagecharles27@gmail.com>
2026-05-15 00:14:19 +0800

    optimize mutations

--- client/src/redux/features/OTPAuth.tsx
+++ client/src/redux/features/OTPAuth.tsx
@@ -1,4 +1,6 @@
-import { api } from "../state/api";
+import { VerifyOTP } from "@/app/(components)/VerifyModal";
+import { api } from "@/redux/state/api";
+import { serverResponse } from "./userAuth";
 
 type Purpose = "VERIFY_EMAIL" | "RESET_PASSWORD" | "LOGIN" | "CHANGE_PASSWORD";
 
@@ -13,17 +15,23 @@ export interface resendOTP {
   email: string;
 }
 
+export interface OTPVerification {
+  message: string;
+  success: boolean;
+  data: VerifyOTP;
+}
+
 export const OTPAuthSlice = api.injectEndpoints({
   endpoints: (builder) => ({
-    verifyOTP: builder.mutation({
-      query: (verify: verifyOTP) => ({
+    verifyOTP: builder.mutation<serverResponse, verifyOTP>({
+      query: (verify) => ({
         url: "/verifyOtp",
         method: "POST",
         body: { ...verify },
       }),
     }),
-    resendOTP: builder.mutation({
-      query: (resendOtp: resendOTP) => ({
+    resendOTP: builder.mutation<OTPVerification, resendOTP>({
+      query: (resendOtp) => ({
         url: "/verifyOtp/resend",
         method: "POST",
         body: { ...resendOtp },
