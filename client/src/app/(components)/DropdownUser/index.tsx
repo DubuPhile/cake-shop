ba587e5671edd33c75943498d148df8b05f0bcd8
Charles <enagecharles27@gmail.com>
2026-05-16 00:05:32 +0800

    implement toast

--- client/src/app/(components)/DropdownUser/index.tsx
+++ client/src/app/(components)/DropdownUser/index.tsx
@@ -5,6 +5,7 @@ import Avatar from "../../../../public/default-avatar.png";
 import { useLogoutMutation } from "@/redux/features/userAuth";
 import { useAppDispatch } from "@/redux/store";
 import { setCredentials } from "@/redux/state/auth";
+import toast from "react-hot-toast";
 
 type UserDropdownProps = {
   user: string | null;
@@ -29,22 +30,24 @@ export default function UserDropdown({ user, avatar }: UserDropdownProps) {
           hasLocalPassword: false,
         }),
       );
+      toast.success("Logout Success!");
     } catch (err) {
       console.log(err);
+      toast.error("Logout Failed!");
     }
   };
 
   return (
-    <div className="relative inline-block w-full max-md:w-[80px]">
+    <div className="relative inline-block w-full max-md:w-20">
       <button
         ref={openRef}
         onClick={() => setOpen((prev) => !prev)}
-        className={`flex w-full p-2 gap-2 items-center justify-center ${
+        className={`flex w-full p-2 gap-2 items-center justify-center cursor-pointer ${
           open ? "rounded-t-[10px] bg-[#262626] text-white" : ""
         }`}
       >
         <Image
-          className="ml-[5px] hidden w-[35px] h-[35px] rounded-full border md:block"
+          className="ml-1.25 hidden w-8.75 h-8.75 rounded-full border md:block"
           src={avatar ?? Avatar}
           width={8.75}
           height={8.75}
@@ -57,21 +60,21 @@ export default function UserDropdown({ user, avatar }: UserDropdownProps) {
         className={`absolute right-0 top-full z-1000 flex w-full flex-col items-end rounded-b-[10px] bg-[#262626] shadow-[0_10px_25px_rgba(0,0,0,0.1)] transition-all duration-200 ${
           open
             ? "visible translate-y-0 opacity-100"
-            : "invisible -translate-y-[10px] opacity-0"
+            : "invisible -translate-y-2.5 opacity-0"
         }`}
       >
-        <button className="flex w-full justify-end bg-transparent px-[14px] py-[10px] text-white hover:rounded-[10px] hover:bg-[hsl(0,0%,25%)]">
+        <button className="cursor-pointer flex w-full justify-end bg-transparent px-3.5 py-2.5 text-white hover:rounded-[10px] hover:bg-[hsl(0,0%,25%)]">
           {" "}
           <Link href="#">Profile</Link>
         </button>
 
-        <button className="flex w-full justify-end bg-transparent px-[14px] py-[10px] text-white hover:rounded-[10px] hover:bg-[hsl(0,0%,25%)]">
+        <button className="cursor-pointer flex w-full justify-end bg-transparent px-3.5 py-2.5 text-white hover:rounded-[10px] hover:bg-[hsl(0,0%,25%)]">
           Settings
         </button>
 
         <button
           onClick={() => handleLogout()}
-          className="flex w-full justify-end bg-transparent px-[14px] py-[10px] text-white hover:rounded-[10px] hover:bg-[hsl(0,0%,25%)]"
+          className="cursor-pointer flex w-full justify-end bg-transparent px-3.5 py-2.5 text-white hover:rounded-[10px] hover:bg-[hsl(0,0%,25%)]"
         >
           Logout
         </button>
