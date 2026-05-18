4b06ec72a7ddfed0e30c875bdb52080f3eae8a95
Charles <enagecharles27@gmail.com>
2026-05-18 19:28:56 +0800

    ads Section

---
+++ client/src/app/(components)/AdsSection/index.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Jill from "../../../../public/jill.jpg";
+import Image, { StaticImageData } from "next/image";
+
+type Ad = {
+  id: number;
+  title: string;
+  description: string;
+  image: string | StaticImageData;
+};
+
+const ads: Ad[] = [
+  {
+    id: 3,
+    title: "SUMMER SALE",
+    description: "Up to 50% off selected items.",
+    image: Jill,
+  },
+];
+
+export default function ProductAdSlider() {
+  const [current, setCurrent] = useState(0);
+
+  // Auto slide
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev === ads.length - 1 ? 0 : prev + 1));
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="relative h-125 overflow-hidden rounded-3xl">
+      {/* Slides */}
+      <div
+        className="flex h-full transition-transform duration-700 ease-in-out"
+        style={{
+          transform: `translateX(-${current * 100}%)`,
+        }}
+      >
+        {ads.map((ad) => (
+          <div key={ad.id} className="relative min-w-full h-full">
+            {/* Background image */}
+            <Image
+              src={ad.image}
+              alt={ad.title}
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+
+            {/* Dark overlay */}
+            <div className="absolute inset-0 bg-black/20" />
+
+            {/* Content */}
+            <div className="relative z-10 flex h-full items-center px-12 ">
+              <div className="max-w-lg text-white bg-black/50 p-5 rounded-2xl">
+                <p className="text-sm uppercase tracking-widest">New Product</p>
+
+                <h1 className="text-5xl font-bold mt-2">{ad.title}</h1>
+
+                <p className="mt-4 text-lg text-gray-200">{ad.description}</p>
+
+                <button className="mt-6 bg-white text-black px-6 py-3 rounded-xl font-semibold hover:scale-105 transition cursor-pointer">
+                  Buy Now
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Dots */}
+      <div className="absolute bottom-5 left-1/2 -translate-x-1/2 flex gap-2">
+        {ads.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => setCurrent(index)}
+            className={`w-3 h-3 rounded-full ${
+              current === index ? "bg-white" : "bg-white/40"
+            }`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
