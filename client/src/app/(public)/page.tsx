50fb16ad8d4573cd016ae34e21898b2a72384108
Charles <enagecharles27@gmail.com>
2026-05-19 01:22:29 +0800

    Optimized

---
+++ client/src/app/(public)/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import AdSlider from "../(components)/AdsSection";
+
+export default function Home() {
+  return (
+    <main>
+      <AdSlider />
+    </main>
+  );
+}
