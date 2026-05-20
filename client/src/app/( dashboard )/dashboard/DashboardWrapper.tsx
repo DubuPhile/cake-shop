4148a44fa940aa57fe4aac03a314a05d1bee0dc3
Charles <enagecharles27@gmail.com>
2026-05-21 00:32:06 +0800

    Dashboard wrapper

---
+++ client/src/app/( dashboard )/dashboard/DashboardWrapper.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Navbar from "@/app/( dashboard )/(dashboardComponents)/NavbarDashboard";
+import Sidebar from "@/app/(components)/Sidebar";
+import { useAppSelector } from "@/redux/store";
+import { useEffect } from "react";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSideBarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  return (
+    <div className={`${isDarkMode ? "dark" : ""}flex w-full `}>
+      <Sidebar />
+      <main
+        className={`flex flex-col w-full h-full py-7 px-2 sm:px-9  ${isSideBarCollapsed ? "md:pl-24" : "md:pl-72"}`}
+      >
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default function DashboardWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <DashboardLayout>{children}</DashboardLayout>;
+}
