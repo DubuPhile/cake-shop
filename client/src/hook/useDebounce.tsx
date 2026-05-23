02daafdbcc919f0d839b2696efba26c0e345ef9e
Charles <enagecharles27@gmail.com>
2026-05-23 22:13:31 +0800

    useDebounce hook

---
+++ client/src/hook/useDebounce.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+interface debounce {
+  value: string;
+  delay: number;
+}
+
+export const useDebounce = ({ value, delay = 500 }: debounce) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(timer); // cleanup
+  }, [value, delay]);
+
+  return debouncedValue;
+};
