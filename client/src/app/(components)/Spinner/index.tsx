1b6b27cd57555b019f55f02819861163c46dd904
Charles <enagecharles27@gmail.com>
2026-05-28 00:13:20 +0800

    Spinner

--- client/src/app/(components)/Spinner/index.tsx
+++ client/src/app/(components)/Spinner/index.tsx
@@ -1,6 +1,6 @@
 export default function Spinner() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-full">
       <div className="animate-spin rounded-full h-10 w-10 border-4 border-gray-300 border-t-black"></div>
     </div>
   );
