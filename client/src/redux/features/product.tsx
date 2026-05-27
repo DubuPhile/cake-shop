9e0daabf9784149a7dc003aaa78a81715ff43d1e
Charles <enagecharles27@gmail.com>
2026-05-28 00:14:53 +0800

    Product injector

---
+++ client/src/redux/features/product.tsx
@@ -0,0 +1,32 @@
+import { api } from "@/redux/state/api";
+
+export interface ProductSize {
+  id: string;
+  size: string;
+  price: number;
+  stock: number;
+}
+
+export interface Products {
+  id: string;
+  name: string;
+  category: string;
+  description: string;
+  image: string;
+  sizes: ProductSize[];
+}
+
+export const AdminAuthSlice = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllProducts: builder.query<Products[], string | void>({
+      query: (search) => ({
+        url: "/product/getAll",
+        method: "GET",
+        params: search ? { search } : {},
+      }),
+      providesTags: ["Products"],
+    }),
+  }),
+});
+
+export const { useGetAllProductsQuery } = AdminAuthSlice;
