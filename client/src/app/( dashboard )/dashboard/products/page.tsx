0219fe907e89731348214148197297cf1baf9a43
Charles <enagecharles27@gmail.com>
2026-05-29 00:07:41 +0800

    select for Category

--- client/src/app/( dashboard )/dashboard/products/page.tsx
+++ client/src/app/( dashboard )/dashboard/products/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { PlusCircleIcon } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "@/app/( dashboard )/(dashboardComponents)/Header";
 import { useGetAllProductsQuery } from "@/redux/features/product";
 import SearchInput from "@/app/(components)/SearchInput";
 import Link from "next/link";
 import { useDebounce } from "@/hook/useDebounce";
 import Spinner from "@/app/(components)/Spinner";
+import Rating from "@/app/(components)/Rating";
 
 type ProductFormData = {
   name: string;
@@ -17,8 +18,9 @@ type ProductFormData = {
 };
 
 export default function Products() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [isModalOpen, setIsModalOpen] = useState<Boolean>(false);
+  const [category, setCategory] = useState<string>("");
   const debounceSearch = useDebounce({ value: searchTerm, delay: 300 });
 
   //const [createProduct] = useCreateProductMutation();
@@ -26,12 +28,9 @@ export default function Products() {
     data: products,
     isLoading,
     isError,
-  } = useGetAllProductsQuery(debounceSearch);
+  } = useGetAllProductsQuery({ search: debounceSearch, category: category });
 
-  const [imagesLoaded, setImagesLoaded] = useState(0);
-
-  const totalImages = products?.length || 0;
-  const allImagesLoaded = totalImages > 0 && imagesLoaded === totalImages;
+  console.log(products);
 
   if (isError || !products) {
     return (
@@ -50,39 +49,59 @@ export default function Products() {
       {/* Header bar */}
       <div className="flex justify-between items-center">
         <Header name="Products" />
-
         <button
-          className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
+          className="flex items-center my-3 bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
           onClick={() => setIsModalOpen(true)}
         >
           <PlusCircleIcon className="w-5 h-5 mr-2 text-gray-200!" />
           Create Product
         </button>
       </div>
-      <SearchInput
-        value={searchTerm}
-        onChange={setSearchTerm}
-        placeholder="Search Product..."
-        className="max-w-100"
-      />
+      <div className="flex gap-5 items-center">
+        <SearchInput
+          value={searchTerm}
+          onChange={setSearchTerm}
+          placeholder="Search Product..."
+          className="max-w-100"
+        />
+
+        <select
+          id="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          className="appearance-none rounded px-3 py-0 h-10 text-gray-50 bg-blue-400 font-semibold drop-shadow-sm my-3 hover:bg-blue-500"
+        >
+          <option className="font-semibold " value="">
+            All Categories
+          </option>
+          <option className="font-semibold" value="cakes">
+            Cake
+          </option>
+          <option className="font-semibold" value="desserts">
+            Desserts
+          </option>
+          <option className="font-semibold" value="birthday cakes">
+            Birthday Cake
+          </option>
+        </select>
+      </div>
+
       {/* BODY PRODUCTS LIST */}
-      {!allImagesLoaded && <Spinner />}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-between">
         {isLoading ? (
-          <div className="py-4">Loading...</div>
+          <Spinner />
         ) : (
           products?.map((product) => (
             <Link
               href={`/dashboard/products/${product.id}`}
               key={product.id}
-              className={`border border-gray-200 dark:border-gray-700 shadow rounded-md p-4 max-w-full w-full mx-auto ${allImagesLoaded ? "block" : "hidden"}`}
+              className={`border border-gray-200 dark:border-gray-700 shadow rounded-md p-4 max-w-full w-full mx-auto `}
             >
               <div className="flex flex-col items-center">
                 <img
                   src={product.image}
                   alt={product.name}
                   className="h-40 object-cover"
-                  onLoad={() => setImagesLoaded((prev) => prev + 1)}
                 />
                 <h3 className="text-lg text-gray-900 dark:text-gray-50 font-semibold ">
                   {product.name}
@@ -93,11 +112,9 @@ export default function Products() {
                 <div className="text-sm text-gray-600 dark:text-gray-400 mt-1">
                   Stock: {product.sizes[0].stock}
                 </div>
-                {/* {product.rating && (
-                  <div className="flex items-center mt-2">
-                    <Rating rating={product.rating} />
-                  </div>
-                )} */}
+                <div className="flex items-center mt-2">
+                  <Rating rating={product.rating} />
+                </div>
               </div>
             </Link>
           ))
