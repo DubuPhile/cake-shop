d5d747cca59d03e928192128a4ad2c83827d4013
Charles <enagecharles27@gmail.com>
2026-06-02 00:11:39 +0800

    create Product routes

--- server/src/routes/productRoutes.ts
+++ server/src/routes/productRoutes.ts
@@ -1,8 +1,22 @@
 import express from "express";
-import { getAllProducts } from "../controllers/productController";
+import {
+  createProduct,
+  getAllProducts,
+} from "../controllers/productController";
+import verifyJWT from "../middleware/verifyJWT";
+import { upload } from "../middleware/multer";
+import { validate } from "../middleware/validate";
+import { createProductSchema } from "../validator/auth.validator";
 
 const router = express.Router();
 
 router.get("/getAll", getAllProducts);
+router.post(
+  "/create",
+  verifyJWT,
+  upload.array("image", 10),
+  validate(createProductSchema),
+  createProduct,
+);
 
 export default router;
