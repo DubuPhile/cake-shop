486dadf4b6eaa826ae74390744fb25dbe49a6755
Charles <enagecharles27@gmail.com>
2026-06-06 00:11:08 +0800

    Rate Product Replies routes

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,9 +1,17 @@
 import express from "express";
-import { RateProduct } from "../controllers/userController";
+import { RateProduct, rateProductReplies } from "../controllers/userController";
 import verifyJWT from "../middleware/verifyJWT";
+import { validate } from "../middleware/validate";
+import { ratingSchema } from "../validator/auth.validator";
 
 const router = express.Router();
 
-router.post("/rate/:Id", verifyJWT, RateProduct);
+router.post("/rate/:Id", verifyJWT, validate(ratingSchema), RateProduct);
+router.post(
+  "/rateReplies/:parentId",
+  verifyJWT,
+  validate(ratingSchema),
+  rateProductReplies,
+);
 
 export default router;
