a456a43d95f2a53a55c3e4de3847275d7b4e6049
Charles <enagecharles27@gmail.com>
2026-06-08 00:13:32 +0800

    validate flattenError

--- server/src/middleware/validate.ts
+++ server/src/middleware/validate.ts
@@ -4,11 +4,11 @@ import { z } from "zod";
 export const validate =
   (schema: z.ZodType) => (req: Request, res: Response, next: NextFunction) => {
     const result = schema.safeParse(req.body);
-
+    console.log(result);
     if (!result.success) {
       return res.status(400).json({
         success: false,
-        errors: result.error.flatten().fieldErrors,
+        errors: z.flattenError(result.error),
       });
     }
 
