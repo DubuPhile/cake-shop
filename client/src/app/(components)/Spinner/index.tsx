1068dfce906d88593ea3806b1b2cab70f2fa0604
Charles <enagecharles27@gmail.com>
2026-06-10 00:13:35 +0800

    Spinner with classname props

--- client/src/app/(components)/Spinner/index.tsx
+++ client/src/app/(components)/Spinner/index.tsx
@@ -1,7 +1,13 @@
-export default function Spinner() {
+type SpinnerProps = {
+  classnames?: string;
+};
+
+export default function Spinner({ classnames = "h-10 w-10" }: SpinnerProps) {
   return (
     <div className="flex items-center justify-center h-full">
-      <div className="animate-spin rounded-full h-10 w-10 border-4 border-gray-300 border-t-black"></div>
+      <div
+        className={`animate-spin rounded-full ${classnames} border-4 border-gray-300 border-t-black`}
+      ></div>
     </div>
   );
 }
