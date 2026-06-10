93ec1974c295da4695cce764ce2edb88aae32cbb
Charles <enagecharles27@gmail.com>
2026-06-11 01:09:09 +0800

    ImageSlice mutations

---
+++ client/src/redux/features/imageSlice.tsx
@@ -0,0 +1,21 @@
+import { api } from "../state/api";
+
+export const imageSlice = api.injectEndpoints({
+  endpoints: (builder) => ({
+    addImage: builder.mutation<void, FormData>({
+      query: (data) => ({
+        url: `/image/${data.get("id")}`,
+        method: "PATCH",
+        body: data,
+      }),
+    }),
+    deleteImage: builder.mutation<void, string>({
+      query: (id) => ({
+        url: `/image/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const { useAddImageMutation, useDeleteImageMutation } = imageSlice;
