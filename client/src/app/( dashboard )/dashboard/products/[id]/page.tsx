115a02305bf8bebc13cc17716d5ab8389de356c7
Charles <enagecharles27@gmail.com>
2026-06-11 01:06:44 +0800

    Image Section

--- client/src/app/( dashboard )/dashboard/products/[id]/page.tsx
+++ client/src/app/( dashboard )/dashboard/products/[id]/page.tsx
@@ -5,7 +5,7 @@ import {
   useGetProductInfoQuery,
   useUpdateProductDetailsMutation,
 } from "@/redux/features/product";
-import { SquarePen } from "lucide-react";
+import { Ban, Save, SquarePen } from "lucide-react";
 import { useParams } from "next/navigation";
 import {
   useCallback,
@@ -17,6 +17,7 @@ import {
 import Category from "./Category";
 import Sizes from "./Sizes";
 import Spinner from "@/app/(components)/Spinner";
+import ImageSection from "@/app/(components)/ImageSection";
 
 export default function ProductPage() {
   const { id } = useParams();
@@ -113,90 +114,94 @@ export default function ProductPage() {
 
   const content = (
     <>
-      <div className="relative flex flex-col">
-        <div className="flex items-center">
-          {!disabled && (
-            <button
-              onClick={() => setDisabled((prev) => !prev)}
-              className="rounded-2xl hover:bg-gray-200 active:bg-gray-300 dark:hover:bg-gray-600  dark:active:bg-gray-700 mx-1 cursor-pointer transition-all duration-300"
-            >
-              <SquarePen className="w-4 h-4 m-2" />
-            </button>
-          )}
-          <input
-            id="name"
-            value={form.name}
-            onChange={(e) =>
-              setForm((prev) => ({
-                ...prev,
-                name: e.target.value,
-              }))
-            }
-            autoComplete="none"
-            disabled={!disabled}
-            className={`text-2xl md:text-4xl font-semibold w-80 rounded-lg px-2 py-1 mr-2 transition-all duration-300 ${disabled ? "bg-white border border-gray-300 dark:bg-[#0000002c] shadow-md" : "bg-none"}`}
-          />
-        </div>
-        <div className="flex items-center mt-5">
-          {!disabled && (
-            <button
-              onClick={() => setDisabled((prev) => !prev)}
-              className="rounded-2xl hover:bg-gray-200 active:bg-gray-300 dark:hover:bg-gray-600  dark:active:bg-gray-700 mx-1 cursor-pointer"
-            >
-              <SquarePen className="w-4  h-4 m-2" />
-            </button>
-          )}
-          <label htmlFor="description" className="font-semibold">
-            Description:
-          </label>
-        </div>
-        <div className={`${!disabled ? "pl-5" : ""}`}>
-          <textarea
-            id="description"
-            ref={textareaRef}
-            value={form.description}
-            onChange={handleDescriptionChange}
-            autoComplete="none"
-            disabled={!disabled}
-            className={`px-2 py-2 w-full min-h-20 max-w-100 lg:max-w-130 text-xs md:text-base indent-8 resize-none rounded-lg transition-all duration-300 ${disabled ? "bg-white border shadow-md border-gray-300 dark:bg-[#0000002c] ml-3" : "bg-none"}`}
-          />
-        </div>
-        <div className="flex items-center">
-          {!disabled && (
-            <button
-              onClick={() => setDisabled((prev) => !prev)}
-              className="rounded-2xl hover:bg-gray-200 active:bg-gray-300 dark:hover:bg-gray-600  dark:active:bg-gray-700 mx-1 cursor-pointer"
-            >
-              <SquarePen className="w-4 h-4 m-2" />
-            </button>
-          )}
-
-          <Category
-            disabled={disabled}
-            setForm={setForm}
-            category={form.category}
-          />
-        </div>
-
-        {disabled && (
-          <div className="flex gap-2 mt-2">
-            <button
-              type="button"
-              onClick={handleCancel}
-              className="shadow-sm bg-gray-300 hover:bg-gray-400 active:bg-gray-500 dark:bg-gray-600 dark:hover:bg-gray-700 dark:active:bg-gray-900 px-3 py-2 rounded-2xl font-semibold cursor-pointer"
-            >
-              Cancel
-            </button>
-            <button
-              type="button"
-              onClick={handleChangeDetails}
-              className="shadow-sm bg-green-300 hover:bg-green-400 active:bg-green-500 dark:bg-green-600 dark:hover:bg-green-700 dark:active:bg-green-900 px-3 py-2 rounded-2xl font-semibold cursor-pointer"
-            >
-              Save Changes
-            </button>
+      <div className="relative flex flex-col max-w-270 items-center md:items-baseline">
+        <div className="flex flex-col w-full sm:w-100 md:w-130 transition-all duration-300">
+          <div className="flex items-center">
+            {!disabled && (
+              <button
+                onClick={() => setDisabled((prev) => !prev)}
+                className="rounded-2xl hover:bg-gray-200 active:bg-gray-300 dark:hover:bg-gray-600  dark:active:bg-gray-700 mx-1 cursor-pointer transition-all duration-300"
+              >
+                <SquarePen className="w-4 h-4 m-2" />
+              </button>
+            )}
+            <input
+              id="name"
+              value={form.name}
+              onChange={(e) =>
+                setForm((prev) => ({
+                  ...prev,
+                  name: e.target.value,
+                }))
+              }
+              autoComplete="none"
+              disabled={!disabled}
+              className={`text-2xl md:text-4xl font-semibold w-70 md:w-100 rounded-lg px-2 py-1 mr-2 transition-all duration-300 ${disabled ? "bg-white border border-gray-300 dark:bg-[#0000002c] shadow-md" : "bg-none"}`}
+            />
           </div>
-        )}
-        <div className="w-100 lg:w-130 transition-all duration-300 mt-2">
+          <div className="flex items-center mt-5">
+            {!disabled && (
+              <button
+                onClick={() => setDisabled((prev) => !prev)}
+                className="rounded-2xl hover:bg-gray-200 active:bg-gray-300 dark:hover:bg-gray-600  dark:active:bg-gray-700 mx-1 cursor-pointer"
+              >
+                <SquarePen className="w-4  h-4 m-2" />
+              </button>
+            )}
+            <label htmlFor="description" className="font-semibold">
+              Description:
+            </label>
+          </div>
+          <div className={`${!disabled ? "pl-5" : ""}`}>
+            <textarea
+              id="description"
+              ref={textareaRef}
+              value={form.description}
+              onChange={handleDescriptionChange}
+              autoComplete="none"
+              disabled={!disabled}
+              className={`px-2 py-2 w-full min-h-20 max-w-100 md:max-w-120 text-xs md:text-base indent-8 resize-none rounded-lg transition-all duration-300 ${disabled ? "bg-white border shadow-md border-gray-300 dark:bg-[#0000002c] ml-3" : "bg-none"}`}
+            />
+          </div>
+          <div className="flex items-center">
+            {!disabled && (
+              <button
+                onClick={() => setDisabled((prev) => !prev)}
+                className="rounded-2xl hover:bg-gray-200 active:bg-gray-300 dark:hover:bg-gray-600  dark:active:bg-gray-700 mx-1 cursor-pointer"
+              >
+                <SquarePen className="w-4 h-4 m-2" />
+              </button>
+            )}
+
+            <Category
+              disabled={disabled}
+              setForm={setForm}
+              category={form.category}
+            />
+          </div>
+
+          {disabled && (
+            <div className="flex gap-2 mt-2">
+              <button
+                type="button"
+                onClick={handleCancel}
+                className="shadow-sm text-xs md:text-base flex items-center justify-center bg-gray-300 hover:bg-gray-400 active:bg-gray-500 dark:bg-gray-600 dark:hover:bg-gray-700 dark:active:bg-gray-900 px-3 py-2 rounded-2xl font-semibold cursor-pointer"
+              >
+                <Ban className="w-4 h-4" />
+                Cancel
+              </button>
+              <button
+                type="button"
+                onClick={handleChangeDetails}
+                className="shadow-sm flex items-center justify-center text-xs md:text-base bg-green-300 hover:bg-green-400 active:bg-green-500 dark:bg-green-600 dark:hover:bg-green-700 dark:active:bg-green-900 px-3 py-2 rounded-2xl font-semibold cursor-pointer"
+              >
+                <Save className="w-4 h-4" />
+                <span>Save</span>
+              </button>
+            </div>
+          )}
+        </div>
+        <div className="w-80 sm:w-100 md:w-130 transition-all duration-300 mt-2">
           <Sizes
             sizes={form.sizes ?? []}
             setSizes={(newSizes) =>
@@ -215,12 +220,21 @@ export default function ProductPage() {
             id={id.toString()}
           />
         </div>
+        <div
+          className={`relative lg:absolute md:top-0 md:right-0 w-80 sm:w-100 md:w-130 lg:w-100 mt-5 transition-all duration-300`}
+        >
+          <ImageSection
+            images={form.images || []}
+            productId={form.id}
+            refetch={refetch}
+          />
+        </div>
       </div>
     </>
   );
 
   return isLoading ? (
-    <div className="h-[100vh]">
+    <div className="h-[85vh]">
       <Spinner />
     </div>
   ) : (
