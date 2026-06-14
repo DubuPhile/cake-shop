57fc6292ddaf95e49aeab553f2aa93605cdeb94e
Charles <enagecharles27@gmail.com>
2026-06-15 00:17:33 +0800

    Token Service

---
+++ server/src/services/token.service.ts
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import { UserInfo } from "../types/token.types";
+
+export const TokenService = {
+  generateAccessToken(user: UserInfo) {
+    return jwt.sign(
+      {
+        UserInfo: {
+          _id: user.userId,
+          user: user.name,
+          isAdmin: user.isAdmin,
+          roles: user.roles ?? [],
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET!,
+      {
+        expiresIn: "15m",
+      },
+    );
+  },
+
+  generateRefreshToken(username: string) {
+    return jwt.sign({ user: username }, process.env.REFRESH_TOKEN_SECRET!, {
+      expiresIn: "1h",
+    });
+  },
+};
