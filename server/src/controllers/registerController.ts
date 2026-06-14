e47645aa66a892ae4bb9b0e998db86dc62637404
Charles <enagecharles27@gmail.com>
2026-06-15 00:15:06 +0800

    refactoring for better reading

--- server/src/controllers/registerController.ts
+++ server/src/controllers/registerController.ts
@@ -1,55 +1,30 @@
 import { Request, Response } from "express";
-import { prisma } from "../../lib/prisma";
-import bcrypt from "bcrypt";
-import { OTPRequest, sendOTP } from "./OTPController";
+import { AuthService } from "../services/auth.service";
 
 export const registerUser = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
   try {
-    const { username, pwd, email } = req.body;
-    if (!username || !pwd || !email) {
+    const body = req.body;
+
+    if (!body.username || !body.pwd || !body.email) {
       res.status(400).json({ message: "All fields are required" });
       return;
     }
-    const existingUser = await prisma.users.findUnique({
-      where: {
-        email: email,
-      },
-    });
-    if (existingUser) {
-      res.status(409).json({ message: "user Already Exist", success: false });
-      return;
-    }
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(pwd, salt);
-    const purpose = "VERIFY_EMAIL";
 
-    const verifyEmail = {
-      name: username,
-      password: hashedPassword,
-      email,
-      purpose,
-    } as OTPRequest;
-    const sendOtp = await sendOTP(verifyEmail);
+    const register = await AuthService.Register(body);
 
-    const OtpData = sendOtp.createdOtp;
-
-    // const newUser = await prisma.users.create({
-    //   data: {
-    //     name: username,
-    //     password: hashedPassword,
-    //     email: email,
-    //   },
-    // });
-
-    res.status(201).send({
+    res.status(200).json({
       message: "Register Successfully",
       success: true,
-      data: OtpData,
+      data: register.OtpData,
     });
-  } catch (err) {
+  } catch (err: any) {
+    if (err.message === "EMAIL_ALREADY_REGISTERED") {
+      res.status(409).json({ message: "User Already Exists", success: false });
+      return;
+    }
     console.log(err);
     res.status(500).json({ message: "Error to register this user" });
   }
