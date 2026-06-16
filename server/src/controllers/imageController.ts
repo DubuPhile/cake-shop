98aa644f000736fec1a7e89ce9e0f8dab1c2be73
Charles <enagecharles27@gmail.com>
2026-06-17 00:04:40 +0800

    refactoring addImage

--- server/src/controllers/imageController.ts
+++ server/src/controllers/imageController.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import { prisma } from "../../lib/prisma";
 import { AuthRequest } from "../middleware/verifyJWT";
 import { bucket } from "../config/firebase";
+import { ProductImageService } from "../services/productImage.service";
+import { productRepo } from "../repositories/product.repository";
+import { UserRepo } from "../repositories/user.repository";
 
 export const addImage = async (
   req: AuthRequest,
@@ -25,60 +28,24 @@ export const addImage = async (
       res.status(401).json({ message: "Unauthorized" });
       return;
     }
-    const user = await prisma.users.findFirst({
-      where: {
-        userId: userId,
-      },
-    });
+    const user = await UserRepo.findbyId(userId);
     if (!user) {
       res.status(404).json({ message: "User not found" });
       return;
     }
 
-    const product = await prisma.product.findFirst({
-      where: {
-        id: id.toString(),
-      },
-    });
+    const product = await productRepo.findProductbyId(id.toString());
 
     if (!product) {
       res.status(400).json({ message: "Product ID not found" });
       return;
     }
 
-    const imageUrls = await Promise.all(
-      files.map(async (file) => {
-        const fileName = `Products/${product.name}/${Date.now()}-${file.originalname}`;
-
-        const fileUpload = bucket.file(fileName);
-
-        await fileUpload.save(file.buffer, {
-          metadata: {
-            contentType: file.mimetype,
-          },
-          public: true,
-        });
-
-        return {
-          url: `https://storage.googleapis.com/${bucket.name}/${fileName}`,
-          filepath: fileName,
-        };
-      }),
+    const imageUrls = await ProductImageService.uploadImage(
+      files,
+      product.name,
     );
-
-    const updatedproduct = await prisma.product.update({
-      where: {
-        id: product.id,
-      },
-      data: {
-        images: {
-          create: imageUrls.map((imgUrl) => ({
-            url: imgUrl.url,
-            path: imgUrl.filepath,
-          })),
-        },
-      },
-    });
+    const updatedproduct = await productRepo.addImage(product.id, imageUrls);
 
     res.status(201).json({
       success: true,
