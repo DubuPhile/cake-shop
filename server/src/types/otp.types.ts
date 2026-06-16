48389735b0d2758c4d67f8a921d3d564b11e2eac
Charles <enagecharles27@gmail.com>
2026-06-17 00:09:56 +0800

    otp types

---
+++ server/src/types/otp.types.ts
@@ -0,0 +1,31 @@
+export type OtpPurpose =
+  | "VERIFY_EMAIL"
+  | "RESET_PASSWORD"
+  | "LOGIN"
+  | "CHANGE_PASSWORD";
+
+export interface OTPRequest extends Request {
+  email: string;
+  name?: string;
+  password?: string;
+  purpose: OtpPurpose;
+}
+
+type createdOtp = {
+  email: string;
+  purpose: OtpPurpose;
+  id: string;
+};
+
+export interface Otp {
+  createdOtp: createdOtp;
+}
+
+export interface VerifyAccounts {
+  email: string;
+  name?: string;
+  password?: string;
+  code: string;
+  purpose: OtpPurpose;
+  expiresAt: Date;
+}
