803e996a4073cd33df9598055407536db5dfdf56
Charles <enagecharles27@gmail.com>
2026-06-20 00:08:30 +0800

    Product types

--- server/src/types/product.types.ts
+++ server/src/types/product.types.ts
@@ -2,3 +2,39 @@ export interface ImageUrl {
   url: string;
   filepath: string;
 }
+
+export interface NewProductData {
+  name: string;
+  category: string;
+  description: string;
+  sizes: Sizes[];
+}
+
+export type Sizes = {
+  id?: string;
+  productId?: string;
+  price: number;
+  size: string;
+  stock: number;
+};
+
+export type Images = {
+  id?: string;
+  productId?: string;
+  url: string;
+  path: string;
+  isPrimary?: boolean;
+};
+
+export interface ProductData {
+  id: string;
+  name: string;
+  category: string;
+  description: string | null;
+  averageRating: number;
+  reviewCount: number;
+  sizes?: Sizes[];
+  images?: Images[];
+  createdAt: Date;
+  updatedAt: Date;
+}
