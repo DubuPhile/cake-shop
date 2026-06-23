3b10e2a9db8173ccc1f24598379fb3c71b794128
Charles <enagecharles27@gmail.com>
2026-06-24 00:56:39 +0800

    rateProduct and replyProduct service

--- server/src/services/product.service.ts
+++ server/src/services/product.service.ts
@@ -1,10 +1,13 @@
 import { bucket } from "../config/firebase";
 import { productRepo } from "../repositories/product.repository";
+import { reviewRepo } from "../repositories/review.repository";
 import { UserRepo } from "../repositories/user.repository";
 import {
   editProductData,
   NewProductData,
   ProductData,
+  RateProd,
+  ReviewData,
 } from "../types/product.types";
 import { ProductImageService } from "./productImage.service";
 
@@ -63,5 +66,58 @@ export const ProductService = {
 
     const updatedProduct = await productRepo.updateProduct(productId, payload);
     return updatedProduct;
+  },
+  /* RATE PRODUCT SERVICE*/
+  rateProduct: async (
+    payload: RateProd,
+    userId: string,
+    productId: string,
+  ): Promise<ReviewData> => {
+    const user = await UserRepo.findbyId(userId);
+    if (!user) throw new Error("USER_NOT_FOUND");
+    const product = await productRepo.findProductbyId(productId);
+    if (!product) throw new Error("PRODUCT_NOT_FOUND");
+
+    const existingReview = await reviewRepo.existingReview(
+      user.userId,
+      product.id,
+    );
+    if (existingReview) throw new Error("REVIEW_EXISTS");
+
+    const reviewData = await reviewRepo.createReview(
+      user.userId,
+      product.id,
+      payload,
+    );
+
+    const UpdateAverageRating = await reviewRepo.updateAvgRating(product.id);
+    console.log(UpdateAverageRating);
+
+    return reviewData;
+  },
+  /* REPLY COMMENT PRODUCT SERVICE*/
+  replyProduct: async (
+    comment: string,
+    userId?: string,
+    parentId?: string,
+  ): Promise<ReviewData> => {
+    if (!userId || !parentId) throw new Error("FORBIDDEN");
+    const user = await UserRepo.findbyId(userId);
+    if (!user) throw new Error("USER_NOT_FOUND");
+    const parentReview = await reviewRepo.findParentById(parentId);
+    if (!parentReview) throw new Error("PARENT_NOT_FOUND");
+
+    const parent = parentReview.parentId;
+
+    if (!parent) throw new Error("PARENT_NOT_FOUND");
+
+    const createReply = await reviewRepo.createReply(
+      comment,
+      parent,
+      parentReview.productId,
+      user.userId,
+    );
+
+    return createReply;
   },
 };
