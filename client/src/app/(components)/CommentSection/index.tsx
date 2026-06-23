fdf3bfe5d29de65b3dc5b2d17fbf266cbc94f9a7
Charles <enagecharles27@gmail.com>
2026-06-24 00:51:02 +0800

    commentSection toggle Likes

--- client/src/app/(components)/CommentSection/index.tsx
+++ client/src/app/(components)/CommentSection/index.tsx
@@ -1,23 +1,36 @@
 "use client";
 
-import { useState } from "react";
-import { ThumbsUp, MessageCircle, Send, Star } from "lucide-react";
+import { useEffect, useState } from "react";
+import { ThumbsUp, MessageCircle, Send } from "lucide-react";
 import { ProductReview } from "@/redux/features/product";
 import defaultImg from "../../../../public/default-avatar.png";
 import Image from "next/image";
 import { TimeInterval } from "@/lib/TimeInterval";
 import StarRating from "../StarRating";
+import { useToggleLikesMutation } from "@/redux/features/reviewSlice";
+import { useAppSelector } from "@/redux/store";
 
 type Props = {
   reviews?: ProductReview[];
 };
 
 export default function CommentSection({ reviews }: Props) {
-  const [comments, setComments] = useState<ProductReview[] | undefined>(
-    reviews,
-  );
+  const [comments, setComments] = useState<ProductReview[] | undefined>();
 
-  const toggleLike = (commentId: string) => {};
+  useEffect(() => {
+    setComments(reviews);
+  }, [reviews]);
+
+  const [toggle] = useToggleLikesMutation();
+
+  const toggleLike = async (commentId: string | undefined) => {
+    if (!commentId) return;
+    try {
+      await toggle(commentId).unwrap();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="max-w-2xl w-full bg-white dark:bg-zinc-900 rounded-xl p-4 shadow space-y-4">
@@ -36,9 +49,13 @@ function CommentItem({
   onLike,
 }: {
   comment: ProductReview;
-  onLike: (id: string) => void;
+  onLike: (id: string | undefined) => void;
 }) {
   const [showReply, setShowReply] = useState(false);
+  const [showReplies, setShowReplies] = useState(false);
+  const { userId } = useAppSelector((state) => state.auth);
+
+  if (!userId) return;
 
   return (
     <div className="flex gap-3">
@@ -69,16 +86,19 @@ function CommentItem({
         {/* Actions */}
         <div className="flex items-center gap-4 mt-1 text-xs text-zinc-500 ml-2">
           <button
-            onClick={() => onLike("")}
-            className="flex items-center gap-1 hover:text-blue-500"
+            onClick={() => onLike(comment.id)}
+            className={`flex items-center gap-1 hover:text-blue-500 transition-all duration-300 cursor-pointer ${comment.likes?.some((like) => like.userId === userId) ? "text-blue-500" : ""}`}
           >
-            <ThumbsUp size={14} />
-            {/* {comment.likes.length} */}
+            <ThumbsUp
+              size={14}
+              className={`transition-all duration-300 ${comment.likes?.some((like) => like.userId === userId) ? "fill-blue-500 scale-110 drop-shadow-sm" : ""}`}
+            />
+            {comment.likesCount}
           </button>
 
           <button
             onClick={() => setShowReply(!showReply)}
-            className="flex items-center gap-1 hover:text-blue-500"
+            className="flex items-center gap-1 hover:text-blue-500 cursor-pointer"
           >
             <MessageCircle size={14} />
             Reply
@@ -89,21 +109,58 @@ function CommentItem({
         {/* Replies */}
         {comment.replies?.length ? (
           <div className="ml-6 mt-3 space-y-3 border-l pl-4 border-zinc-200 dark:border-zinc-700">
-            {comment.replies.map((reply) => (
-              <div key={reply.id} className="flex gap-2">
-                <Image
-                  src={reply.user.avatar ?? defaultImg}
-                  alt="User avatar"
-                  className="w-7 h-7 rounded-full"
-                />
-                <div>
-                  <p className="text-sm font-medium">{reply.user.name}</p>
-                  <p className="text-sm text-zinc-600 dark:text-zinc-300">
-                    {reply.comment}
-                  </p>
-                </div>
-              </div>
-            ))}
+            {!showReplies ? (
+              <button
+                onClick={() => setShowReplies(true)}
+                className="text-sm text-gray-500 mb-2 font-medium cursor-pointer"
+              >
+                View all {comment.replies.length}{" "}
+                {comment.replies.length === 1 ? "reply" : "replies"}
+              </button>
+            ) : (
+              ""
+            )}
+
+            {showReplies
+              ? comment.replies.map((reply) => (
+                  <div key={reply.id}>
+                    <div className="flex gap-2">
+                      <Image
+                        src={reply.user.avatar ?? defaultImg}
+                        alt="User avatar"
+                        className="w-7 h-7 rounded-full"
+                      />
+                      <div>
+                        <p className="text-sm font-medium">{reply.user.name}</p>
+                        <p className="text-sm text-zinc-600 dark:text-zinc-300">
+                          {reply.comment}
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-4 mt-1 text-xs text-zinc-500 ml-2">
+                      <button
+                        onClick={() => onLike(reply.id)}
+                        className={`flex items-center gap-1 hover:text-blue-500 transition-all duration-300 ${reply.likes?.some((like) => like.userId === userId) ? "text-blue-500" : ""} cursor-pointer`}
+                      >
+                        <ThumbsUp
+                          size={14}
+                          className={`transition-all duration-300 ${reply.likes?.some((like) => like.userId === userId) ? "fill-blue-500 scale-110 drop-shadow-sm" : ""}`}
+                        />
+                        {reply.likesCount}
+                      </button>
+
+                      <button
+                        onClick={() => setShowReply(!showReply)}
+                        className="flex items-center gap-1 hover:text-blue-500 cursor-pointer"
+                      >
+                        <MessageCircle size={14} />
+                        Reply
+                      </button>
+                      <span>{TimeInterval(reply.createdAt || "")}</span>
+                    </div>
+                  </div>
+                ))
+              : ""}
           </div>
         ) : null}
 
@@ -115,7 +172,9 @@ function CommentItem({
               placeholder="Write a reply..."
               className="flex-1 px-2 py-1 text-sm rounded-md bg-zinc-100 dark:bg-zinc-800"
             />
-            <button className="text-blue-500 text-sm">Post</button>
+            <button className="text-blue-500 text-sm">
+              <Send className="w-4 h-4" />
+            </button>
           </div>
         )}
       </div>
