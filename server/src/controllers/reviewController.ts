d19a3518edd90982ed5f082ff3f1abf4f35fccc4
Charles <enagecharles27@gmail.com>
2026-06-24 00:53:20 +0800

    move to review Controller

--- server/src/controllers/reviewController.ts
+++ server/src/controllers/reviewController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { AuthRequest } from "../middleware/verifyJWT";
 import { reviewRepo } from "../repositories/review.repository";
+import { RateProd } from "../types/product.types";
+import { ProductService } from "../services/product.service";
 
 export const toggleReviewLike = async (
   req: AuthRequest,
@@ -12,14 +14,14 @@ export const toggleReviewLike = async (
       res.status(401).json({ message: "Unauthorized" });
       return;
     }
-    const { reviewId } = req.params;
+    const { id } = req.params;
 
-    if (!reviewId) {
+    if (!id) {
       res.status(400).json({ message: "Review ID not Found" });
       return;
     }
 
-    const reviewIdString = reviewId.toString();
+    const reviewIdString = id.toString();
 
     const existingLike = await reviewRepo.checkExistLikes(
       userId,
@@ -45,5 +47,86 @@ export const toggleReviewLike = async (
   } catch (err) {
     console.log(err);
     res.status(500).json({ success: false, message: "ERROR ToggleLikes" });
+  }
+};
+
+export const RateProduct = async (
+  req: AuthRequest,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const payload = req.body as RateProd;
+    const userId = req.user?.id;
+    if (!id) throw new Error("PRODUCT_NOT_FOUND");
+    if (!userId) throw new Error("USER_NOT_FOUND");
+
+    const reviewData = await ProductService.rateProduct(
+      payload,
+      userId,
+      id?.toString(),
+    );
+
+    res
+      .status(200)
+      .json({ message: "Review Created!", success: true, data: reviewData });
+  } catch (err) {
+    console.log(err);
+    if (err instanceof Error) {
+      switch (err.message) {
+        case "PRODUCT_NOT_FOUND":
+          res.status(400).json({ success: false, message: err.message });
+          return;
+        case "USER_NOT_FOUND":
+          res.status(401).json({ success: false, message: err.message });
+          return;
+      }
+    }
+    res.status(500).json({ success: false, message: "Rate Product Error!" });
+  }
+};
+
+export const rateProductReplies = async (
+  req: AuthRequest,
+  res: Response,
+): Promise<void> => {
+  try {
+    const userId = req.user?.id;
+    const { id } = req.params;
+    const { comment } = req.body;
+
+    const createReply = await ProductService.replyProduct(
+      comment,
+      userId,
+      id?.toString(),
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Reply to Comment Success!",
+      data: createReply,
+    });
+  } catch (err) {
+    console.log(err);
+    if (err instanceof Error) {
+      switch (err.message) {
+        case "FORBIDDEN":
+          res
+            .status(400)
+            .json({ success: false, message: "Invalid userId or parentId" });
+          return;
+        case "USER_NOT_FOUND":
+          res.status(404).json({ success: false, messsage: "User not found" });
+          return;
+        case "PARENT_NOT_FOUND":
+          res
+            .status(404)
+            .json({ success: false, message: "Parent review not found" });
+          return;
+      }
+    }
+    res
+      .status(500)
+      .json({ success: false, message: "rate Product Replies Error" });
   }
 };
