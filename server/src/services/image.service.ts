0f72d1d80d993a9f340ea4ef0931ccb67ed8ff90
Charles <enagecharles27@gmail.com>
2026-06-26 00:08:37 +0800

    renamed

---
+++ server/src/services/image.service.ts
@@ -0,0 +1,52 @@
+import { bucket } from "../config/firebase";
+import { ImageUrl } from "../types/product.types";
+
+export const ImageService = {
+  uploadProductImage: async (
+    files: Express.Multer.File[],
+    productName: string,
+  ) => {
+    return Promise.all(
+      files.map(async (file) => {
+        const fileName = `Products/${productName}/${Date.now()}-${file.originalname}`;
+
+        const fileUpload = bucket.file(fileName);
+
+        await fileUpload.save(file.buffer, {
+          metadata: {
+            contentType: file.mimetype,
+          },
+          public: true,
+        });
+
+        return {
+          url: `https://storage.googleapis.com/${bucket.name}/${fileName}`,
+          filepath: fileName,
+        };
+      }),
+    );
+  },
+  uploadImgBanner: async (
+    files: Express.Multer.File[],
+  ): Promise<ImageUrl[]> => {
+    return await Promise.all(
+      files.map(async (file) => {
+        const fileName = `Promotional-banners/${Date.now()}-${file.originalname}`;
+
+        const fileUpload = bucket.file(fileName);
+
+        await fileUpload.save(file.buffer, {
+          metadata: {
+            contentType: file.mimetype,
+          },
+          public: true,
+        });
+
+        return {
+          url: `https://storage.googleapis.com/${bucket.name}/${fileName}`,
+          filepath: fileName,
+        };
+      }),
+    );
+  },
+};
