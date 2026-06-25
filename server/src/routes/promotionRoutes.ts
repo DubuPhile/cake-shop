d3fc696d638401a7e421b7bd0ebb338702e57faf
Charles <enagecharles27@gmail.com>
2026-06-26 00:08:06 +0800

    promotionBanner implementation

---
+++ server/src/routes/promotionRoutes.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { createBanner } from "../controllers/promotionController";
+import verifyJWT from "../middleware/verifyJWT";
+import { verifyRoles } from "../middleware/verifyRoles";
+import ROLE_LIST from "../config/roleLists";
+import { upload } from "../middleware/multer";
+import { validate } from "../middleware/validate";
+import { PromotionSchema } from "../validator/auth.validator";
+
+const router = express.Router();
+
+router.post(
+  "/create",
+  verifyJWT,
+  verifyRoles(ROLE_LIST.Admin),
+  upload.single("images"),
+  validate(PromotionSchema),
+  createBanner,
+);
+
+export default router;
