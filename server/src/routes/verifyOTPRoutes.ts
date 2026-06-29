9106e6ab089b96122c7472a3f94d83921e398165
Charles <enagecharles27@gmail.com>
2026-06-29 17:58:19 +0800

    implementation Throttle

--- server/src/routes/verifyOTPRoutes.ts
+++ server/src/routes/verifyOTPRoutes.ts
@@ -2,10 +2,11 @@ import express from "express";
 import { resendOTP, verifyOTP } from "../controllers/OTPController";
 import { validate } from "../middleware/validate";
 import { resendOtp, verifyOtpSchema } from "../validator/auth.validator";
+import { throttle } from "../middleware/throttle";
 
 const router = express.Router();
 
 router.post("/", validate(verifyOtpSchema), verifyOTP);
-router.post("/resend", validate(resendOtp), resendOTP);
+router.post("/resend", throttle(60000), validate(resendOtp), resendOTP);
 
 export default router;
