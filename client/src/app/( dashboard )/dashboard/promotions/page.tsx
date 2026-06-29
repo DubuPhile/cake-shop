b2455b46eba8ff5000bd3faf0ad242079a609802
Charles <enagecharles27@gmail.com>
2026-06-30 00:28:52 +0800

    unfished promotion

--- client/src/app/( dashboard )/dashboard/promotions/page.tsx
+++ client/src/app/( dashboard )/dashboard/promotions/page.tsx
@@ -1,39 +1,24 @@
 "use client";
 
-import { StaticImageData } from "next/image";
 import { useState } from "react";
-import Jill from "../../../../../public/jill.jpg";
-import { CoverEditor } from "@/app/(components)/DraggableImage";
-
-type Ad = {
-  id: number;
-  title: string;
-  description: string;
-  image: string | StaticImageData;
-  x: number;
-  y: number;
-};
-
-const ads: Ad[] = [
-  {
-    id: 3,
-    title: "SUMMER SALE",
-    description: "Up to 50% off selected items.",
-    image: Jill,
-    x: 0,
-    y: 0,
-  },
-];
+import PromoBannerModal from "../../(dashboardComponents)/PromoBannerModal";
 
 export default function Promotions() {
-  const [offsetY, setOffsetY] = useState<number>(0);
-  const [imgBanner, setImgBanner] = useState<File>();
-
-  console.log(offsetY, imgBanner);
-
+  const [createBannerModal, setCreateBannerModal] = useState<boolean>(false);
   return (
     <div>
-      <CoverEditor setOffsetY={setOffsetY} setImgBanner={setImgBanner} />
+      <button
+        onClick={() => setCreateBannerModal(true)}
+        className="cursor-pointer"
+      >
+        Create Banner
+      </button>
+      {createBannerModal && (
+        <PromoBannerModal
+          isOpen={createBannerModal}
+          onClose={() => setCreateBannerModal(false)}
+        />
+      )}
     </div>
   );
 }
