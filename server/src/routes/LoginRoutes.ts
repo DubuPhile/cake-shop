4df3a7993ca8ee67bcd62400c4afa6c2000a9e2c
Charles <enagecharles27@gmail.com>
2026-06-29 17:58:06 +0800

    implementation LoginLimiter

--- server/src/routes/LoginRoutes.ts
+++ server/src/routes/LoginRoutes.ts
@@ -2,9 +2,17 @@ import express from "express";
 import { loginUser } from "../controllers/loginController";
 import { validate } from "../middleware/validate";
 import { loginSchema } from "../validator/auth.validator";
+import { loginLimiter } from "../middleware/rateLimiter";
+import { throttle } from "../middleware/throttle";
 
 const router = express.Router();
 
-router.post("/", validate(loginSchema), loginUser);
+router.post(
+  "/",
+  throttle(1000),
+  loginLimiter,
+  validate(loginSchema),
+  loginUser,
+);
 
 export default router;
