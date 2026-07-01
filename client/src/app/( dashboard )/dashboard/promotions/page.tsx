72b24511995e907558cb2df0b6d05be0dc2aa719
Charles <enagecharles27@gmail.com>
2026-07-02 00:15:37 +0800

    implement partially promo modal

--- client/src/app/( dashboard )/dashboard/promotions/page.tsx
+++ client/src/app/( dashboard )/dashboard/promotions/page.tsx
@@ -2,9 +2,37 @@
 
 import { useState } from "react";
 import PromoBannerModal from "../../(dashboardComponents)/PromoBannerModal";
+import { useCreateBannerMutation } from "@/redux/features/bannerSlice";
+import toast from "react-hot-toast";
 
 export default function Promotions() {
   const [createBannerModal, setCreateBannerModal] = useState<boolean>(false);
+  const [createBanner] = useCreateBannerMutation();
+
+  const handleCreateBanner = async (data: FormData) => {
+    for (const [key, value] of data.entries()) {
+      console.log(key, value);
+    }
+    try {
+      await createBanner(data).unwrap();
+
+      toast.success("Banner Created", {
+        style: {
+          fontWeight: "600",
+          color: "green",
+        },
+      });
+      setCreateBannerModal(false);
+    } catch (err) {
+      console.log(err);
+      toast.error("Create Banner Failed", {
+        style: {
+          fontWeight: "600",
+          color: "red",
+        },
+      });
+    }
+  };
   return (
     <div>
       <button
@@ -17,6 +45,7 @@ export default function Promotions() {
         <PromoBannerModal
           isOpen={createBannerModal}
           onClose={() => setCreateBannerModal(false)}
+          onSubmit={handleCreateBanner}
         />
       )}
     </div>
