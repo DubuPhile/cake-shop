780b59c772e9333ad45a43bb12eac9e9979b19fc
Charles <enagecharles27@gmail.com>
2026-07-02 00:16:54 +0800

    partial public

--- client/src/app/(public)/page.tsx
+++ client/src/app/(public)/page.tsx
@@ -1,10 +1,51 @@
-import Link from "next/link";
+"use client";
+
 import AdSlider from "../(components)/AdsSection";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../(components)/ui/accordion";
+import { useGetBannerQuery } from "@/redux/features/bannerSlice";
 
 export default function Home() {
+  const { data: banner } = useGetBannerQuery();
+
+  const now = new Date();
+
+  const visibleBanner = banner?.filter((item) => {
+    if (!item.startDate || !item.endDate) return false;
+    return new Date(item.startDate) <= now && new Date(item.endDate) >= now;
+  });
+
   return (
-    <main>
-      <AdSlider />
+    <main className="flex flex-col items-center">
+      <div className=" w-full max-w-7xl">
+        <AdSlider banner={visibleBanner} />
+
+        <div className="w-full md:w-100">
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>What is React?</AccordionTrigger>
+
+              <AccordionContent>
+                React is a JavaScript library for building user interfaces.
+              </AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="item-2">
+              <AccordionTrigger>What is Next.js?</AccordionTrigger>
+
+              <AccordionContent>
+                Next.js is a React framework for production applications.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </div>
+
+      {/* <CreateProductModal /> */}
     </main>
   );
 }
