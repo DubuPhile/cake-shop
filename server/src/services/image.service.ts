c9c8df02df88581123b354f2e082485571443ba2
Charles <enagecharles27@gmail.com>
2026-07-08 00:08:46 +0800

    uploadReviewImg

--- server/src/services/image.service.ts
+++ server/src/services/image.service.ts
@@ -26,6 +26,30 @@ export const ImageService = {
       }),
     );
   },
+  uploadReviewImage: async (
+    files: Express.Multer.File[],
+    productName: string,
+  ) => {
+    return Promise.all(
+      files.map(async (file) => {
+        const fileName = `Products/${productName}/ReviewImg/${Date.now()}-${file.originalname}`;
+
+        const fileUpload = bucket.file(fileName);
+
+        await fileUpload.save(file.buffer, {
+          metadata: {
+            contentType: file.mimetype,
+          },
+          public: true,
+        });
+
+        return {
+          url: `https://storage.googleapis.com/${bucket.name}/${fileName}`,
+          filepath: fileName,
+        };
+      }),
+    );
+  },
   uploadImgBanner: async (
     files: Express.Multer.File[],
   ): Promise<ImageUrl[]> => {
