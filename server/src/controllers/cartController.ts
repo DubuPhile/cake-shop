e7241491c5221b3b83b0284ea4a1312066a605d9
Charles <enagecharles27@gmail.com>
2026-07-09 00:12:51 +0800

    getCart Controller

--- server/src/controllers/cartController.ts
+++ server/src/controllers/cartController.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 import { AuthRequest } from "../middleware/verifyJWT";
 import { CartItem } from "../types/cart.types";
 import { CartService } from "../services/cart.service";
+import { CartRepo } from "../repositories/cart.repository";
 
 export const addToCart = async (
   req: AuthRequest,
@@ -19,5 +20,23 @@ export const addToCart = async (
   } catch (err) {
     console.log(err);
     res.status(500).json({ success: false, message: "Server Error addToCart" });
+  }
+};
+
+export const getCarts = async (
+  req: AuthRequest,
+  res: Response,
+): Promise<void> => {
+  try {
+    const userId = req.user?.id;
+    if (!userId) throw new Error("USER404");
+    const carts = await CartRepo.getCart(userId);
+
+    res.status(200).json(carts);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ success: false, messsage: "Server Error in GetCart" });
   }
 };
