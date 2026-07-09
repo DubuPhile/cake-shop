affb0ea71df33b3c9321107e40abadb5b9362ee7
Charles <enagecharles27@gmail.com>
2026-07-10 00:14:13 +0800

    delete routes

--- server/src/routes/cartRoutes.ts
+++ server/src/routes/cartRoutes.ts
@@ -1,12 +1,13 @@
 import express from "express";
-import { addToCart, getCarts } from "../controllers/cartController";
+import { addToCart, deleteCart, getCarts } from "../controllers/cartController";
 import verifyJWT from "../middleware/verifyJWT";
 import { validate } from "../middleware/validate";
-import { AddToCartSchema } from "../validator/auth.validator";
+import { AddToCartSchema, idSchema } from "../validator/auth.validator";
 
 const router = express.Router();
 
 router.post("/create", verifyJWT, validate(AddToCartSchema), addToCart);
 router.get("/", verifyJWT, getCarts);
+router.delete("/:id", verifyJWT, validate(idSchema, "params"), deleteCart);
 
 export default router;
