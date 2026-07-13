de29ef01f1fa79d2d0591a675d7894b9f0122325
Charles <enagecharles27@gmail.com>
2026-07-14 00:27:31 +0800

    redis clearProductBySlug

--- server/src/utils/redisCache.ts
+++ server/src/utils/redisCache.ts
@@ -1,9 +1,13 @@
 import { redis } from "../config/redis";
 
-export const clearProductCache = async () => {
+export const clearProductsCache = async () => {
   const keys = await redis.keys("products:*");
 
   if (keys.length > 0) {
     await redis.del(keys);
   }
+};
+
+export const clearProductBySlug = async (slug: string) => {
+  await redis.del(`product:${slug}`);
 };
