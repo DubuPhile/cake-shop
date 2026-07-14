e3f5fa239b26049d8b77a1c1a509b7ffa5059ad6
Charles <enagecharles27@gmail.com>
2026-07-15 00:33:43 +0800

    Dashboard

--- client/src/app/( dashboard )/dashboard/page.tsx
+++ client/src/app/( dashboard )/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
 import TotalUser from "../(dashboardComponents)/TotalUser";
+import PendingOrders from "../(dashboardComponents)/PendingOrders";
+import MonthlySales from "../(dashboardComponents)/MonthlySales";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -8,8 +10,12 @@ export const metadata: Metadata = {
 
 export default function Dashboard() {
   return (
-    <div>
-      <TotalUser />
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+      <div className="grid grid-cols-3 gap-2 md:gap-4">
+        <TotalUser />
+        <PendingOrders />
+        <MonthlySales />
+      </div>
     </div>
   );
 }
