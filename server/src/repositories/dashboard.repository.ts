f9867881b4eee0bbe46b8a8d9137f64ee5ca0469
Charles <enagecharles27@gmail.com>
2026-07-15 00:40:32 +0800

    Dashboard repo

--- server/src/repositories/dashboard.repository.ts
+++ server/src/repositories/dashboard.repository.ts
@@ -5,22 +5,22 @@ export const DashboardRepo = {
     return prisma.users.count();
   },
 
-  //   getTotalOrders: async () => {
-  //     return prisma.orders.count();
-  //   },
+  getTotalOrders: async () => {
+    return prisma.order.count();
+  },
 
-  //   getTotalRevenue: async () => {
-  //     const revenue = await prisma.orders.aggregate({
-  //       _sum: {
-  //         totalAmount: true,
-  //       },
-  //       where: {
-  //         status: "COMPLETED",
-  //       },
-  //     });
+  getTotalRevenue: async () => {
+    const revenue = await prisma.order.aggregate({
+      _sum: {
+        totalAmount: true,
+      },
+      where: {
+        status: "COMPLETED",
+      },
+    });
 
-  //     return revenue._sum.totalAmount || 0;
-  //   },
+    return revenue._sum.totalAmount || 0;
+  },
 
   getUsersThisMonth: async (startDate: Date) => {
     return prisma.users.count({
@@ -39,6 +39,42 @@ export const DashboardRepo = {
           gte: startDate,
           lt: endDate,
         },
+      },
+    });
+  },
+
+  getOrderThisMonth: async (startDate: Date) => {
+    return prisma.order.count({
+      where: {
+        createdAt: {
+          gte: startDate,
+        },
+      },
+    });
+  },
+
+  getOrderLastMonth: async (startDate: Date, endDate: Date) => {
+    return prisma.order.count({
+      where: {
+        createdAt: {
+          gte: startDate,
+          lt: endDate,
+        },
+      },
+    });
+  },
+
+  getMonthlySales: async (startDate: Date, endDate: Date) => {
+    return prisma.order.aggregate({
+      where: {
+        status: "COMPLETED",
+        createdAt: {
+          gte: startDate,
+          lte: endDate,
+        },
+      },
+      _sum: {
+        totalAmount: true,
       },
     });
   },
