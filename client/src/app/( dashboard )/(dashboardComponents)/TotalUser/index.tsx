5fc697496b20529e04a2053571f45869463dbe9f
Charles <enagecharles27@gmail.com>
2026-07-15 00:33:26 +0800

    Total User Component

--- client/src/app/( dashboard )/(dashboardComponents)/TotalUser/index.tsx
+++ client/src/app/( dashboard )/(dashboardComponents)/TotalUser/index.tsx
@@ -2,30 +2,34 @@
 
 import { useGetDashboardStatsQuery } from "@/redux/features/dashboardSlice";
 import { TrendingDown, TrendingUp } from "lucide-react";
+import Link from "next/link";
 
 export default function TotalUser() {
   const { data } = useGetDashboardStatsQuery();
 
   return (
-    <div className="bg-gray-100 dark:bg-gray-700 rounded-2xl p-5 w-35 md:w-50">
-      <h2 className=" text-xs md:text-lg">Total Users</h2>
+    <Link
+      href={"/dashboard/users"}
+      className="bg-gray-100 dark:bg-gray-700 rounded-2xl p-3 md:p-5 flex flex-col justify-center"
+    >
+      <h2 className=" text-[8px] sm:text-lg">Users</h2>
       <div className="flex items-center">
-        <h4 className="pl-1 font-extrabold text-base md:text-2xl">
+        <h4 className="pl-1 font-extrabold text-[10px] sm:text-2xl">
           {data?.totalUsers}
         </h4>
-        {data?.growthPercentage && (
+        {data?.userGrowthPercentage && (
           <span
-            className={`text-xs ${data?.growthPercentage >= 0 ? "text-green-500" : "text-red-500"} flex items-center ml-2`}
+            className={`text-[10px] sm:text-sm ${data?.userGrowthPercentage >= 0 ? "text-green-500" : "text-red-500"} flex items-center ml-2`}
           >
-            {data?.growthPercentage! >= 0 ? (
-              <TrendingUp className="w-4 h-4 mr-1" />
+            {data?.userGrowthPercentage! >= 0 ? (
+              <TrendingUp className="w-3 md:w-4 h-4 mr-1" />
             ) : (
-              <TrendingDown className="w-4 h-4 mr-1" />
+              <TrendingDown className="w-3 md:w-4 h-4 mr-1" />
             )}
-            {Math.abs(data?.growthPercentage)}%
+            {Math.abs(data?.userGrowthPercentage)}%
           </span>
         )}
       </div>
-    </div>
+    </Link>
   );
 }
