1f1614e6fb974eee21aebc7bffc958f69b9127d6
Charles <enagecharles27@gmail.com>
2026-07-16 00:06:55 +0800

    MonthlySales

--- client/src/app/( dashboard )/(dashboardComponents)/MonthlySales/index.tsx
+++ client/src/app/( dashboard )/(dashboardComponents)/MonthlySales/index.tsx
@@ -1,30 +1,30 @@
 "use client";
 
-import { useGetDashboardStatsQuery } from "@/redux/features/dashboardSlice";
+import { useGetLatestStatsQuery } from "@/redux/features/dashboardSlice";
 import { TrendingDown, TrendingUp } from "lucide-react";
 
 export default function MonthlySales() {
-  const { data } = useGetDashboardStatsQuery();
+  const { data } = useGetLatestStatsQuery();
   return (
-    <div className="bg-gray-100 dark:bg-gray-700 rounded-2xl p-3 md:p-5 flex flex-col justify-center">
-      <h2 className=" text-[8px] sm:text-lg">Monthly Sales</h2>
+    <div className="bg-gray-100 dark:bg-gray-700 rounded-2xl p-3 md:p-5 flex flex-col justify-center shadow">
+      <h2 className=" text-xs sm:text-lg">Monthly Sales</h2>
       <div className="flex items-center flex-wrap ">
-        <h4 className="sm:pl-1 font-bold text-[10px] sm:text-2xl">
+        <h4 className="sm:pl-1 font-bold text-[10px] sm:text-lg">
           {new Intl.NumberFormat("en-PH", {
             style: "currency",
             currency: "PHP",
-          }).format(data?.monthlySales || 0)}
+          }).format(data?.stats.totalSales || 0)}
         </h4>
-        {data?.salesGrowth && (
+        {data?.stats.salesGrowth && (
           <span
-            className={`text-[10px] sm:text-sm ${data?.salesGrowth >= 0 ? "text-green-500" : "text-red-500"} flex items-center sm:ml-2`}
+            className={`text-[10px] sm:text-sm ${data?.stats.salesGrowth >= 0 ? "text-green-500" : "text-red-500"} flex items-center sm:ml-2`}
           >
-            {data?.salesGrowth! >= 0 ? (
+            {data?.stats.salesGrowth! >= 0 ? (
               <TrendingUp className="w-3 md:w-4 h-4 mr-1" />
             ) : (
               <TrendingDown className="w-3 md:w-4 h-4 mr-1" />
             )}
-            {Math.abs(data?.salesGrowth)}%
+            {Math.abs(data?.stats.salesGrowth)}%
           </span>
         )}
       </div>
