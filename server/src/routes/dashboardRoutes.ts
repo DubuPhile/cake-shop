99e1dd55576b1b3ec5c44e1f22c6426928426b81
Charles <enagecharles27@gmail.com>
2026-07-16 00:02:59 +0800

    new Dashboard Controller

--- server/src/routes/dashboardRoutes.ts
+++ server/src/routes/dashboardRoutes.ts
@@ -2,15 +2,19 @@ import express from "express";
 import verifyJWT from "../middleware/verifyJWT";
 import { verifyRoles } from "../middleware/verifyRoles";
 import ROLE_LIST from "../config/roleLists";
-import { getDashboardStats } from "../controllers/dashboardController";
+import {
+  getLatestStats,
+  getMonthlyStats,
+} from "../controllers/dashboardController";
 
 const router = express.Router();
 
+router.get("/stats", verifyJWT, verifyRoles(ROLE_LIST.Admin), getLatestStats);
 router.get(
-  "/stats",
+  "/monthly",
   verifyJWT,
   verifyRoles(ROLE_LIST.Admin),
-  getDashboardStats,
+  getMonthlyStats,
 );
 
 export default router;
