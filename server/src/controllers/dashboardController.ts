532f8dcf654f0de4cac94de165bd538602a1a282
Charles <enagecharles27@gmail.com>
2026-07-16 00:02:47 +0800

    new Dashboard Controller

--- server/src/controllers/dashboardController.ts
+++ server/src/controllers/dashboardController.ts
@@ -1,21 +1,36 @@
 import { Response } from "express";
 import { AuthRequest } from "../middleware/verifyJWT";
+import { MonthlyStatsRepo } from "../repositories/monthlyStats.repository";
+import { UserRepo } from "../repositories/user.repository";
 
-import { DashboardService } from "../services/dashboard.service";
-
-export const getDashboardStats = async (
+export const getLatestStats = async (
   req: AuthRequest,
   res: Response,
 ): Promise<void> => {
   try {
-    const stats = await DashboardService.getDashboardStats();
+    const stats = await MonthlyStatsRepo.latestStats();
+    const totalUsers = await UserRepo.totalUser();
 
-    res.status(200).json(stats);
-  } catch (error) {
-    console.error(error);
+    res.status(200).json({ stats, totalUsers });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error in latestStats" });
+  }
+};
 
-    res.status(500).json({
-      message: "Internal Server Error",
-    });
+export const getMonthlyStats = async (
+  req: AuthRequest,
+  res: Response,
+): Promise<void> => {
+  try {
+    const result = await MonthlyStatsRepo.monthlyStats();
+    if (result.length === 0) {
+      res.status(404).json({ message: "Monthly Stats Table Empty!" });
+      return;
+    }
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error in monthlyStats" });
   }
 };
