a0f73957841d6a03807f1d8d61f8408fa2e5f8f7
Charles <enagecharles27@gmail.com>
2026-07-16 00:07:20 +0800

    TotalUser

--- client/src/app/( dashboard )/(dashboardComponents)/TotalUser/index.tsx
+++ client/src/app/( dashboard )/(dashboardComponents)/TotalUser/index.tsx
@@ -1,32 +1,32 @@
 "use client";
 
-import { useGetDashboardStatsQuery } from "@/redux/features/dashboardSlice";
+import { useGetLatestStatsQuery } from "@/redux/features/dashboardSlice";
 import { TrendingDown, TrendingUp } from "lucide-react";
 import Link from "next/link";
 
 export default function TotalUser() {
-  const { data } = useGetDashboardStatsQuery();
-
+  const { data } = useGetLatestStatsQuery();
+  console.log(data);
   return (
     <Link
       href={"/dashboard/users"}
-      className="bg-gray-100 dark:bg-gray-700 rounded-2xl p-3 md:p-5 flex flex-col justify-center"
+      className="bg-gray-100 dark:bg-gray-700 rounded-2xl p-3 md:p-5 flex flex-col justify-center shadow"
     >
-      <h2 className=" text-[8px] sm:text-lg">Users</h2>
+      <h2 className=" text-xs sm:text-lg">Users</h2>
       <div className="flex items-center">
-        <h4 className="pl-1 font-extrabold text-[10px] sm:text-2xl">
+        <h4 className="pl-1 font-extrabold text-[12px] sm:text-2xl">
           {data?.totalUsers}
         </h4>
-        {data?.userGrowthPercentage && (
+        {data?.stats.usersGrowth && (
           <span
-            className={`text-[10px] sm:text-sm ${data?.userGrowthPercentage >= 0 ? "text-green-500" : "text-red-500"} flex items-center ml-2`}
+            className={`text-[10px] sm:text-sm ${data?.stats.usersGrowth >= 0 ? "text-green-500" : "text-red-500"} flex items-center ml-2`}
           >
-            {data?.userGrowthPercentage! >= 0 ? (
+            {data?.stats.usersGrowth! >= 0 ? (
               <TrendingUp className="w-3 md:w-4 h-4 mr-1" />
             ) : (
               <TrendingDown className="w-3 md:w-4 h-4 mr-1" />
             )}
-            {Math.abs(data?.userGrowthPercentage)}%
+            {Math.abs(data?.stats.usersGrowth)}%
           </span>
         )}
       </div>
