3d01b4715f25ae03cf7014317c1f4f9af1748273
Charles <enagecharles27@gmail.com>
2026-07-20 01:31:40 +0800

    optimize Order Status type

--- server/src/types/order.Type.ts
+++ server/src/types/order.Type.ts
@@ -1,7 +1,10 @@
-export type OrderStatus =
-  | "PENDING"
-  | "PAID"
-  | "PROCESSING"
-  | "READY"
-  | "COMPLETED"
-  | "CANCELLED";
+export const ORDER_STATUSES = [
+  "PENDING",
+  "PAID",
+  "PROCESSING",
+  "READY",
+  "COMPLETED",
+  "CANCELLED",
+] as const;
+
+export type OrderStatus = (typeof ORDER_STATUSES)[number];
