20b1bd01641872322f1fd2ce0184baefff0efcfe
Charles <enagecharles27@gmail.com>
2026-07-20 01:31:21 +0800

    optimize getOrderByStatusTotal routes

--- server/src/routes/orderRoutes.ts
+++ server/src/routes/orderRoutes.ts
@@ -1,5 +1,8 @@
 import express from "express";
-import { createOrder } from "../controllers/orderController";
+import {
+  createOrder,
+  getTotalOrderPerStatus,
+} from "../controllers/orderController";
 import verifyJWT from "../middleware/verifyJWT";
 import { validate } from "../middleware/validate";
 import { createOrderSchema } from "../validator/auth.validator";
@@ -7,5 +10,6 @@ import { createOrderSchema } from "../validator/auth.validator";
 const router = express.Router();
 
 router.post("/create", verifyJWT, validate(createOrderSchema), createOrder);
+router.get("/count-by-status", verifyJWT, getTotalOrderPerStatus);
 
 export default router;
