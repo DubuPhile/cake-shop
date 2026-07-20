454f7249b4e684067f23b966dd43be794ac162ef
Charles <enagecharles27@gmail.com>
2026-07-21 00:05:07 +0800

    shadcn/ui

--- client/src/lib/utils.tsx
+++ client/src/lib/utils.tsx
@@ -1,21 +1,6 @@
-import { MyCart } from "@/redux/features/cartSlice";
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const calculateTotalPrice = (items: MyCart[]) => {
-  if (!Array.isArray(items)) return 0;
-
-  return items.reduce((total, item) => {
-    return total + (item.quantity || 0) * (item.size?.price || 0);
-  }, 0);
-};
-
-export const calculateTotalItems = (items: MyCart[]) => {
-  if (!Array.isArray(items)) return 0;
-
-  return items.reduce((total, item) => total + (item.quantity || 0), 0);
-};
