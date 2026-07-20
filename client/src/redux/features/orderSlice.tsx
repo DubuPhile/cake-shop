524ada7ca273b699f245f2be2d89cdd22f5264a9
Charles <enagecharles27@gmail.com>
2026-07-21 00:07:11 +0800

    get Recent Orders slice

--- client/src/redux/features/orderSlice.tsx
+++ client/src/redux/features/orderSlice.tsx
@@ -1,19 +1,32 @@
 import { api } from "@/redux/state/api";
+import { NumberOfItems } from "./product";
 
 type CartIds = {
   cartItemIds: (string | undefined)[];
 };
 
+export type Status =
+  | "PENDING"
+  | "PAID"
+  | "PROCESSING"
+  | "READY"
+  | "COMPLETED"
+  | "CANCELLED";
+
 type CountByStatus = {
-  status:
-    | "PENDING"
-    | "PAID"
-    | "PROCESSING"
-    | "READY"
-    | "COMPLETED"
-    | "CANCELLED";
+  status: Status;
+
   value: number;
   color: string;
+};
+
+type RecentOrders = {
+  id?: string;
+  orderId: string;
+  customer: string;
+  total: number;
+  status: Status;
+  date: Date;
 };
 
 export const OrderSlice = api.injectEndpoints({
@@ -32,7 +45,19 @@ export const OrderSlice = api.injectEndpoints({
         method: "GET",
       }),
     }),
+    getRecentOrders: builder.query<RecentOrders[], NumberOfItems | void>({
+      query: (params) => ({
+        url: "/order/recent-orders",
+        method: "GET",
+        params: { take: params?.take },
+      }),
+      providesTags: ["ORDERS"],
+    }),
   }),
 });
 
-export const { useCreateOrderMutation, useGetCountByStatusQuery } = OrderSlice;
+export const {
+  useCreateOrderMutation,
+  useGetCountByStatusQuery,
+  useGetRecentOrdersQuery,
+} = OrderSlice;
