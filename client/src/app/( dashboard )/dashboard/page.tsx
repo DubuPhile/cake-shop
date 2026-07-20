16080abe41e58e9bbf9d56d18e975f99464b9645
Charles <enagecharles27@gmail.com>
2026-07-21 00:05:53 +0800

    incomplete dashboard

--- client/src/app/( dashboard )/dashboard/page.tsx
+++ client/src/app/( dashboard )/dashboard/page.tsx
@@ -1,20 +1,30 @@
-import { Metadata } from "next";
+"use client";
 import TotalUser from "../(dashboardComponents)/TotalUser";
-import PendingOrders from "../(dashboardComponents)/PendingOrders";
+import MonthlyOrders from "../(dashboardComponents)/MonthlyOrders";
 import MonthlySales from "../(dashboardComponents)/MonthlySales";
-
-export const metadata: Metadata = {
-  title: "Dashboard",
-  description: "Metrics",
-};
+import SalesSummaryCard from "./SalesSummaryCard";
+import { useGetBestSellingProductQuery } from "@/redux/features/product";
+import BestSellingCard from "./BestSellingCard";
+import OrderStatusCard from "./OrderStatusCard";
+import RecentOrders from "./RecentOrders";
 
 export default function Dashboard() {
+  const { data } = useGetBestSellingProductQuery();
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
-      <div className="grid grid-cols-3 gap-2 md:gap-4">
-        <TotalUser />
-        <PendingOrders />
-        <MonthlySales />
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 ">
+        <div className="grid grid-cols-3 gap-2 md:gap-4 ">
+          <TotalUser />
+          <MonthlyOrders />
+          <MonthlySales />
+          <SalesSummaryCard />
+        </div>
+        <RecentOrders />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
+        <OrderStatusCard />
+        <BestSellingCard />
+        <h4 className="bg-yellow-500 h-80">helll</h4>
       </div>
     </div>
   );
